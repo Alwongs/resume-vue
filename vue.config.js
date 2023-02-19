c6628bf8f20777d983dcab0107993b81d2d25868
Alex <Alwong@ya.ru>
2023-02-19 22:33:33 +0400

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === "production" ? "/resume-vue/" : "/",
+    // publicPath: process.env.NODE_ENV === "production" ? "/resume-vue/" : "/",
+    publicPath: process.env.BASE_URL === "production" ? "/resume-vue/" : "/",
     transpileDependencies: true,
     css: {
         loaderOptions: {
